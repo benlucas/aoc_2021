57f6791ad708f3e0295581732171cc8a045d1176
Benjamin Lucas <ben@benlucas.co.uk>
2021-12-05 12:09:59 +0000

    day 1

---
+++ src/day1/app.ts
@@ -0,0 +1,28 @@
+import { readFileSync, createReadStream } from 'fs';
+import * as readline from 'readline';
+
+const values = readFileSync('src/day1/input.txt', 'utf-8')
+    .split("\n")
+    .map(strValue => Number(strValue));
+
+function partOne() {
+    const increases = values.filter((value, index, array) => value > array[index-1]).length;
+
+    console.log(`Part 1: ${increases}`);
+}
+
+function partTwo() {
+    let increases = 0;
+    let previousWindowValue = Number.POSITIVE_INFINITY;
+
+    for (let i = 2; i < values.length; i++) {
+        const windowValue = values[i] + values[i - 1] + values[i - 2];
+        if (windowValue > previousWindowValue) increases++;
+        previousWindowValue = windowValue;
+    }
+
+    console.log(`Part 2: ${increases}`);
+}
+
+partOne();
+partTwo();
